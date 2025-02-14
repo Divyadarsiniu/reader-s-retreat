9c73ea0d2a656c1d11447786a3d4f3120a9a4fe9
Divyadarsiniu <157292647+Divyadarsiniu@users.noreply.github.com>
2025-02-14 09:34:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
 //create popup-overlay , popup-box,add-popup-button and set it as block
-var popupoverlay=document.querySelector(".popup-overlay")
-var popupbox=document.querySelector(".popup-box")
-var btn=document.getElementById("add-popup-button")
-
-btn.addEventListener("click",function(){
-   
-    popupoverlay.style.display="block"
-    popupbox.style.display="block"
-}
-)
-//cancel btn
-var cancelpopup=document.querySelector(".cancel-popup")
+document.addEventListener("DOMContentLoaded",function(){
+    var popupoverlay=document.querySelector(".popup-overlay")
+    var popupbox=document.querySelector(".popup-box")
+    var btn=document.getElementById("add-popup-button")
+    
+    btn.addEventListener("click",function(){
+       
+        popupoverlay.style.display="block"
+        popupbox.style.display="block"
+    })
+    //cancel btn
+var cancelpopup=document.getElementById("cancel-popup")
 cancelpopup.addEventListener("click",function(event){
     event.preventDefault()
+
     popupoverlay.style.display="none"
     popupbox.style.display="none"
+
 })
 //add btn with book-title-input,book-author-input,book-description-input
+
+
 var container=document.querySelector(".container")
-var addinput=document.getElementById("add-input")
+var addbook=document.getElementById("add-book")
 var booktitleinput=document.getElementById("book-title-input")
 var bookauthorinput=document.getElementById("book-author-input")
-var bookdescriptioninput=document.getElementById("book-descrription-input")
+var bookdescriptioninput=document.getElementById("book-description-input")
+
+
 //event listener
-    container.addEventListener("click",function(event){
+
+
+    addbook.addEventListener("click",function(event){
     event.preventDefault()
     var div=document.createElement("div")
     div.setAttribute("class","book-container")
+
     div.innerHTML=` <h2>${booktitleinput.value}</h2>
             <h5>${bookauthorinput.value}</h5>
             <p> ${bookdescriptioninput.value}</p>
-            <button onclick='deletebook(event)'>Delete</button>`
+            <button class="delete-book">Delete</button>`
+            
             container.append(div)
             popupoverlay.style.display="none"
             popupbox.style.display="none"
+
+            // Clear input fields after adding the book
+        booktitleinput.value = "";
+        bookauthorinput.value = "";
+        bookdescriptioninput.value = "";
+})
+container.addEventListener("click",function(event){
+    if(event.target && event.target.classList.contains("delete-book")){
+        event.target.parentElement.remove()
+    }
+})
 })
 
-function deletebook(event){
-    event.deletebook.parentElement.remove()
-}
+
